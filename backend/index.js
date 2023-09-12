87819ee621eaecb052e64ee6286b1f18c0d43585
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2023-09-13 03:27:53 +0530

    setup mongo connection

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
 const PORT = 4000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-
+const connect = () => {
+    mongoose.
+    connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch((err) => {
+        throw err;
+    });
+};
 
 
 app.listen(PORT, () => {
-
+    connect();
     console.log("listening on port 4000");
 });
+
